34482e35f0f397c2d4a6aba395d96e0e5d75b75f
Logan McAnsh <logan@mcan.sh>
2018-02-26 16:19:02 -0500

    base config
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ base.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['import', 'prettier'],
+  rules: {
+    'no-param-reassign': [2, { props: false }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true
+      }
+    ]
+  }
+};
