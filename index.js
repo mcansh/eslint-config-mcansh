c83e57da554470f136e91be926469938850c4ae2
Logan McAnsh <logan@mcan.sh>
2018-02-26 15:20:05 -0500

    🤙
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-closing-tag-location': 0,
+    'react/jsx-curly-brace-presence': 0,
+    'no-param-reassign': [2, { props: false }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true
+      }
+    ]
+  }
+};
