1c0d94c4a2f4fb3ec9bf4daa7bfcc089e7b7736a
Logan McAnsh <logan@mcan.sh>
2018-04-25 21:00:30 -0400

    next.js' link component is weird and will cause eslint to throw this error

--- index.js
+++ index.js
@@ -19,5 +19,13 @@ module.exports = {
         singleQuote: true,
       },
     ],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
   },
 };
