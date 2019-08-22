e922b64f95a41c3c6284fe69bf9220073993a732
Logan McAnsh <logan@mcan.sh>
2019-08-21 23:54:11 -0400

    chore: add rule for next's link component
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ module.exports = {
     'kentcdodds/jsx-a11y',
     'kentcdodds/jest',
   ],
+  rules: {
+    // fixes a false positive for next.js link component https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
 };
