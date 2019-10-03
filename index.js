0dc70d9b27a41ce5376baf65c72d388b717ed744
Logan McAnsh <logan@mcan.sh>
2019-10-03 12:55:10 -0400

    update rules, include root import eslint settings
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ module.exports = {
     'prettier/react',
   ],
   rules: {
+    complexity: 'off',
+    'one-var': ['error', 'never'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'no-underscore-dangle': ['error', { allow: ['__NEXT_DATA__'] }],
+
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
@@ -22,6 +27,8 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton'],
       },
     ],
-    'react/jsx-pascal-case': 'off',
+
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/extensions': ['error', 'never', { svg: 'always', json: 'always' }],
   },
 };
