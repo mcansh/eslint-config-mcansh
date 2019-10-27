f968f2087b18780729fceeb132c740f5100b9b3b
Logan McAnsh <logan@mcan.sh>
2019-10-27 12:26:17 -0400

    chore: update stylelint config for nullishCoalescingOperator

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,28 @@
 module.exports = {
-  processors: ['stylelint-processor-styled-components'],
+  processors: [
+      'stylelint-processor-styled-components',
+      {
+        moduleName: 'styled-components',
+        importName: 'default',
+        strict: false,
+        ignoreFiles: [],
+        parserPlugins: [
+          'jsx',
+          'objectRestSpread',
+          ['decorators', { decoratorsBeforeExport: true }],
+          'classProperties',
+          'exportExtensions',
+          'asyncGenerators',
+          'functionBind',
+          'functionSent',
+          'dynamicImport',
+          'optionalCatchBinding',
+          'optionalChaining',
+          'nullishCoalescingOperator',
+        ],
+      },
+    ],
+  ],
   extends: [
     'stylelint-config-recommended',
     'stylelint-config-styled-components',
