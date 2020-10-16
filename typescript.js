67d534e692a27c1f5aa3e79b27c1c3807dc2975e
Logan McAnsh <logan@mcan.sh>
2020-10-16 11:19:22 -0400

    chore: enforce `import type` imports

--- typescript.js
+++ typescript.js
@@ -29,9 +29,23 @@ module.exports = {
       },
       rules: {
         'react/prop-types': 'off', // handled by using React.FC<Props>
+        
+        /**
+          * enforce `import type` style imports
+        */
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports' },
+        ],
+        'no-duplicate-imports': 'off',
+        '@typescript-eslint/no-duplicate-imports': 'error',
 
-        'no-use-before-define': 'off', // https://github.com/typescript-eslint/typescript-eslint/issues/2502
+        /**
+          * resolves https://github.com/typescript-eslint/typescript-eslint/issues/2502
+        */
+        'no-use-before-define': 'off',
         '@typescript-eslint/no-use-before-define': 'error',
+        
         '@typescript-eslint/prefer-nullish-coalescing': 'error',
         '@typescript-eslint/prefer-optional-chain': 'error',
         '@typescript-eslint/explicit-member-accessibility': 'error',
